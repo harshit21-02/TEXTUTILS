a524c1d74f21f36978d71050f043b418a028083d
Harshit <c.harshit2122@gmail.com>
2022-04-27 19:17:52 +0530

    Addded More Features like Capitalize whole text and Remove Extra Spaces

--- textutils/views.py
+++ textutils/views.py
@@ -1,6 +1,9 @@
 #I have created this
+
+import enum
 from pickle import GET
 import re
+from tkinter import ON
 from django.http import HttpResponse
 from django.shortcuts import render
 def index(request):
@@ -9,14 +12,37 @@ def index(request):
 def analyze(request):
     djtext = request.GET.get('text','default') 
     removpunctext=request.GET.get('removpunc','off') 
-    print(removpunctext)
+    capitaltext=request.GET.get('capitalize','off') 
+    extratext=request.GET.get('extraspaces','off') 
     analyzed=""
-    punctuations='''!@#$%^&*,./()<>?{}|\[]:";'-'''
-    for char in djtext:
-        if char not in punctuations:
-            analyzed+=char 
-        
-    params = {'purpose':'Removed Punctuations','AnalyzedText':analyzed} 
+    ctext=""
+    final="" 
+    if removpunctext == 'on':
+        punctuations='''!@#$%^&*,./()<>?{}|\[]:";'-'''
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed+char 
+    else:
+        analyzed=analyzed+djtext  
+
+    
+    if capitaltext == 'on':
+        for char in analyzed:
+            ctext=ctext+char.upper()
+    else:
+        ctext=ctext+analyzed
+
+    if extratext == 'on':
+        for index,char in enumerate(ctext):
+            if ctext[index]==" " and ctext[index+1]==" ":
+                pass 
+            else:
+                final=final+char
+    else:   
+        for index,char in enumerate(ctext):
+            if ctext[index]==" " and ctext[index+1]==" ":
+                final=final+char
+    params = {'purpose':'Your Analyzed Text: ','AnalyzedText':ctext} 
     return render(request,'analyze.html', params)
 
 
